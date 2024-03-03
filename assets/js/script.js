d72e7c4755876a15c66b04864a4c0cfbf0c338c1
Yongo David <davidyongo9@gmail.com>
2024-03-03 15:59:01 +0100

    Home swiper update/need to resolve

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,3 +26,25 @@ $(document).ready(function(){
         $('.navbar-box').removeClass("navbar-box-show");
     })
 });
+
+
+// HOME SWIPER SECTION 
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    grabCursor: true,
+    slidesPreview: 'auto',
+    centeredSlides: 'auto',
+
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction: false ,
+    },
+
+    breakpoints: {
+        1220:{
+            spaceBetween: -32,
+        }
+    }
+  })
+   
