bd3ed75014cbbb93fad975e836cb4db694b6279e
Yongo David <davidyongo9@gmail.com>
2024-03-20 04:20:45 +0100

    books form update

--- Client/Form.js
+++ Client/Form.js
@@ -1,5 +1,4 @@
 const form = document.querySelector('.join__form');
-
 // Add an event listener for form submission
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -20,4 +19,27 @@ form.addEventListener('submit', function(event) {
         messageElement.style.color = "red"; // Style the message in red
     }
 });
+
+// Select the form element
+const bookForm = document.querySelector('.book-form');
+bookForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const emailInput = bookForm.querySelector('.books_input');
+    const email = emailInput.value;
+    const messageElement = bookForm.querySelector('.message2');
+    if (email.includes('@') && email.includes('.com')) {
+        // Display the success message
+        messageElement.textContent = "Email sent";
+        messageElement.style.color = "green"; 
+        emailInput.value = "";
+
+        // Optionally, you can submit the form here
+        // bookForm.submit();
+    } else {
+        // Display the error message
+        messageElement.textContent = "Please enter a valid email address";
+        messageElement.style.color = "red"; // Style the message in red
+    }
+});
+
 
