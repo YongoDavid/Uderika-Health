1c2ba3bf220fa454f76097f8c503c20657828c21
Yongo David <davidyongo9@gmail.com>
2024-03-21 03:53:00 +0100

    another download link test

--- Client/download.js
+++ Client/download.js
@@ -1,5 +1,8 @@
 document.getElementById('download3').addEventListener('click', function() {
-    // Replace 'YOUR_GOOGLE_DRIVE_PDF_LINK' with the actual shareable link of your PDF file
     let pdfUrl = 'https://drive.google.com/uc?id=1UN9xUxIWrxs-eDXVOeVPkaaF5cN3atMP&export=download';
     window.open(pdfUrl, '_blank');
+
+    setTimeout(function() {
+        window.location.href = 'YOUR_MAIN_PAGE_URL';
+    }, 3000); // 3000 milliseconds = 3 seconds
 });
