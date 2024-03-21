5ae9faaabe16e03d4fd183b9a996331fcc0e5864
Yongo David <davidyongo9@gmail.com>
2024-03-21 04:59:22 +0100

    Books link update

--- Client/download.js
+++ Client/download.js
@@ -1,4 +1,46 @@
+document.getElementById('download1').addEventListener('click', function() {
+    let pdfUrl = 'https://drive.google.com/uc?id=1UN9xUxIWrxs-eDXVOeVPkaaF5cN3atMP&export=download';
+    window.open(pdfUrl, '_blank');
+
+    setTimeout(function() {
+        window.location.href = 'index.html';
+    }, 1000); // 3000 milliseconds = 3 seconds
+});
+document.getElementById('download2').addEventListener('click', function() {
+    let pdfUrl = 'https://drive.google.com/uc?id=1UN9xUxIWrxs-eDXVOeVPkaaF5cN3atMP&export=download';
+    window.open(pdfUrl, '_blank');
+
+    setTimeout(function() {
+        window.location.href = 'index.html';
+    }, 1000); // 3000 milliseconds = 3 seconds
+});
+
 document.getElementById('download3').addEventListener('click', function() {
+    let pdfUrl = 'https://drive.google.com/uc?id=1UN9xUxIWrxs-eDXVOeVPkaaF5cN3atMP&export=download';
+    window.open(pdfUrl, '_blank');
+
+    setTimeout(function() {
+        window.location.href = 'index.html';
+    }, 1000); // 3000 milliseconds = 3 seconds
+});
+
+document.getElementById('download4').addEventListener('click', function() {
+    let pdfUrl = 'https://drive.google.com/uc?id=1UN9xUxIWrxs-eDXVOeVPkaaF5cN3atMP&export=download';
+    window.open(pdfUrl, '_blank');
+
+    setTimeout(function() {
+        window.location.href = 'index.html';
+    }, 1000); // 3000 milliseconds = 3 seconds
+});
+document.getElementById('download5').addEventListener('click', function() {
+    let pdfUrl = 'https://drive.google.com/uc?id=1UN9xUxIWrxs-eDXVOeVPkaaF5cN3atMP&export=download';
+    window.open(pdfUrl, '_blank');
+
+    setTimeout(function() {
+        window.location.href = 'index.html';
+    }, 1000); // 3000 milliseconds = 3 seconds
+});
+document.getElementById('download6').addEventListener('click', function() {
     let pdfUrl = 'https://drive.google.com/uc?id=1UN9xUxIWrxs-eDXVOeVPkaaF5cN3atMP&export=download';
     window.open(pdfUrl, '_blank');
 
