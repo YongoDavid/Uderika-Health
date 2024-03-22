928d5641431fcb0c87efe7c72142d915428dcaf1
Yongo David <davidyongo9@gmail.com>
2024-03-22 17:59:37 +0100

    file download reset

--- Client/download.js
+++ Client/download.js
@@ -1,50 +1,8 @@
-document.getElementById('download1').addEventListener('click', function() {
-    let pdfUrl = 'https://drive.google.com/uc?id=1lODIfdg76bAmpkSqLbFt-ku2KAszJTuA&export=download';
-    window.open(pdfUrl, '_blank');
-
-    // setTimeout(function() {
-    //     window.location.href = 'index.html';
-    // }, 1000); // 3000 milliseconds = 3 seconds
-});
-document.getElementById('download2').addEventListener('click', function() {
-    let pdfUrl = 'https://drive.google.com/uc?id=14YyZdwiF2pKs7k638va8QJJOHgOo6cmM&export=download';
-    window.open(pdfUrl, '_blank');
-
-    // setTimeout(function() {
-    //     window.location.href = 'index.html';
-    // }, 1000); // 3000 milliseconds = 3 seconds
-});
-
-document.getElementById('download3').addEventListener('click', function() {
-    let pdfUrl = 'https://drive.google.com/uc?id=1UN9xUxIWrxs-eDXVOeVPkaaF5cN3atMP&export=download';
-    window.open(pdfUrl, '_blank');
-
-    // setTimeout(function() {
-    //     window.location.href = 'index.html';
-    // }, 1000); // 3000 milliseconds = 3 seconds
-});
-
-document.getElementById('download4').addEventListener('click', function() {
-    let pdfUrl = 'https://drive.google.com/uc?id=1t0vJo5Y64QFz58PxphHTk-gSS7g2d36D&export=download';
-    window.open(pdfUrl, '_blank');
-
-    // setTimeout(function() {
-    //     window.location.href = 'index.html';
-    // }, 1000); // 3000 milliseconds = 3 seconds
-});
-document.getElementById('download5').addEventListener('click', function() {
-    let pdfUrl = 'https://drive.google.com/uc?id=1KmoyMXxEyU1aBi6IDU4GAhuxjIsCAwTU&export=download';
-    window.open(pdfUrl, '_blank');
-
-    // setTimeout(function() {
-    //     window.location.href = 'index.html';
-    // }, 1000); // 3000 milliseconds = 3 seconds
-});
-document.getElementById('download6').addEventListener('click', function() {
-    let pdfUrl = 'https://drive.google.com/uc?id=16YLvNfKFpBXI2smg0DihUAwZ1FRoqyws&export=download';
-    window.open(pdfUrl, '_blank');
-
-    // setTimeout(function() {
-    //     window.location.href = 'index.html';
-    // }, 1000); // 3000 milliseconds = 3 seconds
-});
+// document.getElementById('download1').addEventListener('click', function() {
+//     let pdfUrl = 'https://drive.google.com/uc?id=1lODIfdg76bAmpkSqLbFt-ku2KAszJTuA&export=download';
+//     window.open(pdfUrl, '_blank');
+// });
+// document.getElementById('download1').addEventListener('click', function() {
+//     let pdfUrl = 'https://drive.google.com/uc?export=download&id=1lODIfdg76bAmpkSqLbFt-ku2KAszJTuA';
+//     window.open(pdfUrl, '_blank');
+// });
