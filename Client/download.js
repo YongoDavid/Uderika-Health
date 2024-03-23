9f9e3b2c5caba9fc9487b21ad00f78aa2577f452
Yongo David <davidyongo9@gmail.com>
2024-03-23 21:52:10 +0100

    Books links files still not working

--- Client/download.js
+++ Client/download.js
@@ -1,28 +1,30 @@
-// document.getElementById('download1').addEventListener('click', function() {
-//     let pdfUrl = 'https://drive.google.com/uc?id=1lODIfdg76bAmpkSqLbFt-ku2KAszJTuA&export=download';
-//     window.open(pdfUrl, '_blank');
-// });
-// document.getElementById('download1').addEventListener('click', function() {
-//     let pdfUrl = 'https://drive.google.com/uc?export=download&id=1lODIfdg76bAmpkSqLbFt-ku2KAszJTuA';
-//     window.open(pdfUrl, '_blank');
-// });
-
-// BOOK1 LINK insale lies
+// BOOK1 LINK insale lies 
 document.getElementById('download5').addEventListener('click' , function () {
-    let pdfuRL = 'https://drive.google.com/uc?id=1lODIfdg76bAmpkSqLbFt-ku2KAszJTuA&export=download';
+    let pdfuRL = 'https://drive.google.com/uc?id=1KmoyMXxEyU1aBi6IDU4GAhuxjIsCAwTU&export=download';
     window.open(pdfuRL)
 });
-
-// BOOK1 LINK hidden truth
-// This is the only oone working ( i need to figure it out)
-document.getElementById('download1').addEventListener('click' , function () {
-    let pdfuRL = 'https://drive.google.com/uc?export=download&id=1KmoyMXxEyU1aBi6IDU4GAhuxjIsCAwTU';
-    window.open(pdfuRL)
+// BOOK2 LINK first aid NOT WORKING 
+document.getElementById('download2').addEventListener('click' , function () {
+    let pdfurl = 'https://drive.google.com/uc?export=download&id=14YyZdwiF2pKs7k638va8QJJOHgOo6cmM';
+    window.open(pdfurl);
 });
-
-// BOOK2 LINK 
-// document.getElementById('download2').addEventListener('submit' , function () {
-//     let pdfUrl = 'https://drive.google.com/uc?id=14YyZdwiF2pKs7k638va8QJJOHgOo6cmM&export=download'
-//     window.open(pdfUrl , '_blank')
-// })
-
+// BOOKS5 LINK Hidden truth NOT WORKING
+document.getElementById('download1').addEventListener('click', function() {
+    let pdfUrl = 'https://drive.google.com/file/d/1lODIfdg76bAmpkSqLbFt-ku2KAszJTuA/view?usp=sharing';
+    window.open(pdfUrl);
+});
+// BOOKS3 LINK food for diabetic   NOT WORKING
+document.getElementById('download3').addEventListener('click' , function() {
+    let pdfUrl = 'https://drive.google.com/uc?export=download&id=14YyZdwiF2pKs7k638va8QJJOHgOo6cmM';
+    window.open(pdfUrl);
+});
+// BOOKS4 LINK untold truths NOT WORKING
+document.getElementById('download4').addEventListener('click' , function (){
+    let pdfUrl = 'https://drive.google.com/uc?id=1t0vJo5Y64QFz58PxphHTk-gSS7g2d36D&export=download';
+    window.open(pdfUrl);
+});
+// BOOK6 LINK habit traker NOT WORKING
+document.getElementById('download6').addEventListener('click', function(){
+    let pdfUrl = 'https://drive.google.com/uc?id=16YLvNfKFpBXI2smg0DihUAwZ1FRoqyws&export=download';
+    window.open(pdfUrl)
+});
