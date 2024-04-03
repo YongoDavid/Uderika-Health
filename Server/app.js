988a4d53927b2d28e36eead7ddbdbbb945aec70f
Yongo David <davidyongo9@gmail.com>
2024-04-03 02:39:46 +0100

    hide env and modules files

--- Server/app.js
+++ Server/app.js
@@ -19,7 +19,7 @@ app.use((req,res, next)=>{
 
 app.use(
     cors({
-        origin: ["http://127.0.0.1:5500" , "https://uderika-health-server.onrender.com"],
+        origin: ["http://127.0.0.1:5500" ,],
         credentials: true
     })
 )
