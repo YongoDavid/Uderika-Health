968e91c4559f0068f960140932542524b60a84cf
Yongo David <davidyongo9@gmail.com>
2024-04-06 17:40:10 +0100

    testing new connection with MDB atlas

--- Server/app.js
+++ Server/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
 const path = require('path');
-const db = require('./config/db'); // Import database connection setup
-const cors = require('cors')
+// const db = require('./config/db'); // Import database connection setup
+const cors = require('cors');
+const mongoose = require('mongoose');
+const {Schema } = mongoose;
+const dbURL = 'mongodb+srv://davidyongo9:ksV8Avu0F2iB5h26@uderika-emails.plbqwuf.mongodb.net/';
+mongoose.connect(dbURL)
+//         .then((result)=>{app.listen(5500)})
+//         .catch((err)=>{console.log(err)});
 // const uuid = require('uuid')
 const app = express();
 
@@ -13,26 +19,45 @@ app.use(express.urlencoded({extended:true}));
 app.use(bodyParser.json());
 // // CUSTOM MIDDLEWARE 
 app.use((req,res, next)=>{
-    console.log('Server running on LIVE SERVER')
+    console.log('Server running on LIVE')
     next()
 });
-
 app.use(
     cors({
-        origin: ["http://127.0.0.1:5500" , "https://stormy-hare-hem.cyclic.app"],
+        origin: ["http://127.0.0.1:5500" ,],
         credentials: true
     })
 )
+app.use(express.json());
+
+
+// EMAIL SCHEMA 
+const Sch = new Schema({
+   email: String,
+   id: Number
+})
+const Email = mongoose.model('Email' , Sch);
+// module.exports = Email;
+
+
 // EMAIL ROUTE
-app.post('/Email', (req, res) => {
-    const { email} = req.body;
-    // for email 
-    // console.log('Email received:', email);
-    res.sendStatus(200); // Send a response to the client
-    // const id = uuid.v4()
-    if (!email ) {
-        return res.status(400).json({ error: 'Invalid email address' });
-    }
+app.post('/Email', async(req, res) => {
+   const data = new Email({
+    id: req.body.id,
+    email: req.body.email
+   });
+
+   const Info = data.save()
+   res.send('posted')
+});
+    // const { email} = req.body;
+    // // for email 
+    // // console.log('Email received:', email);
+    // res.sendStatus(200); // Send a response to the client
+    // // const id = uuid.v4()
+    // if (!email ) {
+    //     return res.status(400).json({ error: 'Invalid email address' });
+    // }
 
     // ANOTHER WAY TO CONNECT TO DB 
     // const insertQuery = ' INSERT INTO Uderika`.`Email` (`Emails`) VALUES (?)' 
@@ -46,11 +71,11 @@ app.post('/Email', (req, res) => {
     // });
    
     // SIMPLE WAY TO CONNECT TO DB 
-    let sql = `INSERT INTO Emails (Emails)
-    VALUES ('${email}')
-    `
-    db.execute(sql)
-});
+    // let sql = `INSERT INTO Emails (Emails)
+    // VALUES ('${email}')
+    // `
+    // db.execute(sql)
+
 
 // Error handling middleware
 app.use((err, req, res, next) => {
---
+++ Server/model/Email.js
@@ -0,0 +1,10 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema();
+
+// const emailSchema = new Schema({
+//    email: request.body.email,
+//    id: req.body.id
+// })
+
+// const Email = mongoose.model('Blog' , emailSchema);
+// module.exports = Email;
