0d20f633b8783f7f08bd983665fe9c1b086d2382
Yongo David <davidyongo9@gmail.com>
2024-04-14 14:45:39 +0100

    text server update

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs')
 const morgan = require('morgan');
 require('dotenv').config()
 const bodyParser = require('body-parser')
@@ -11,7 +12,7 @@ app.use(bodyParser.json());
 // // CUSTOM MIDDLEWARE 
 app.use(
     cors({
-        origin: ["http://127.0.0.1:3306" , "https://uderika-health.onrender.com"],
+        origin: ["http://127.0.0.1:5500" , "https://uderika-health.onrender.com"],
         credentials: true
     })
 )
@@ -22,7 +23,19 @@ app.use((req,res, next)=>{
 });
 app.use(express.static(path.join(__dirname, '../Client')));
 
+app.post('/Email',(req,res)=>{
+  const email = req.body.email;
 
+  fs.appendFile('emails.txt',email + '\n' ,(err) => {
+    if(err){
+      console.error('Error saving email:', err);
+      res.status(500).send('Error saving email');
+    } else {
+      console.log('Email saved:', email);
+      res.sendStatus(200);
+    }
+  });
+});
 
 
 // START THE SERVER  
