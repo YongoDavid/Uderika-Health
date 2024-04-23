dd6abdbe2fa87193bdd4e663509f6920be2bcdbc
Yongo David <davidyongo9@gmail.com>
2024-04-23 06:14:08 +0100

    timestamp update

--- Server/index.js
+++ Server/index.js
@@ -63,7 +63,7 @@ app.get('/emails', (req, res) => {
 app.post('/Email', (req, res) => {
   const email = req.body.email;
   const timestamp = new Date().toDateString();
-  const data = `${email}\n  Date::${timestamp}\n`;
+  const data = `${email}\n  Date::${timestamp}`;
 
   fs.appendFile('emails.txt', data, (err) => {
     if (err) {
