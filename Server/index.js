aa241cac777ccbbaacccfd3fbf7284f051f6e19f
Yongo David <davidyongo9@gmail.com>
2024-04-24 04:13:14 +0100

    fetch update

--- Server/index.js
+++ Server/index.js
@@ -64,8 +64,7 @@ app.get('/emails', (req, res) => {
 app.post('/Email', (req, res) => {
   const email = req.body.email;
   const timestamp = new Date().toDateString();
-  // const data = `${email}   Date:${timestamp}\n`;
-  const data = {email , timestamp};
+  const data = `${email}   Date:${timestamp}\n`;
 
   fs.appendFile('emails.txt', data, (err) => {
     if (err) {
